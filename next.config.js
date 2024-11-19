4d2aaaf758c52a3a00e1b360cbf3e00936a38d22
Julia Voortman <julia19@outlook.com>
2024-11-19 22:48:24 +0000

    Second commit eh

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   images: {
     domains: [], // Add any image domains you need to use
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 }
