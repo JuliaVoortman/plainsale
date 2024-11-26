132bb1999ad1c5dbff3297b6880aadc04d491e07
Julia Voortman <julia19@outlook.com>
2024-11-26 01:46:58 +0000

    Maybe

--- components/providers/view-mode-provider.tsx
+++ components/providers/view-mode-provider.tsx
@@ -1,36 +1,23 @@
-// components/providers/view-mode-provider.tsx
 "use client";
 
 import { Provider } from 'jotai';
 import { useHydrateAtoms } from 'jotai/utils';
-import { viewModeAtom, companyBrandingAtom, type ViewMode, type BackgroundConfig } from '@/lib/atoms';
+import { viewModeAtom, type ViewMode } from '@/lib/atoms';
 
 interface ViewModeProviderProps {
   children: React.ReactNode;
   initialMode?: ViewMode;
-  initialBranding?: BackgroundConfig;
 }
 
-function HydrateAtoms({ 
-  initialMode = 'internal',
-  initialBranding = {
-    type: 'color',
-    color: '#002447',
-    animation: 'none'
-  },
-  children 
-}: ViewModeProviderProps) {
-  useHydrateAtoms([
-    [viewModeAtom, initialMode],
-    [companyBrandingAtom, initialBranding]
-  ]);
+function HydrateAtoms({ initialMode = 'internal', children }: ViewModeProviderProps) {
+  useHydrateAtoms([[viewModeAtom, initialMode]]);
   return <>{children}</>;
 }
 
-export function ViewModeProvider({ children, initialMode, initialBranding }: ViewModeProviderProps) {
+export function ViewModeProvider({ children, initialMode }: ViewModeProviderProps) {
   return (
     <Provider>
-      <HydrateAtoms initialMode={initialMode} initialBranding={initialBranding}>
+      <HydrateAtoms initialMode={initialMode}>
         {children}
       </HydrateAtoms>
     </Provider>
